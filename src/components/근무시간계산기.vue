8d37b9723f33848add08f83ad5c23a273a60f69e
jhkim39 <jhkim39@cowave.kr>
2026-04-30 13:12:01 +0900

    fix. 근무 시간에 따른 휴게시간 맞춤 수정

--- src/components/근무시간계산기.vue
+++ src/components/근무시간계산기.vue
@@ -116,8 +116,7 @@ const 총체류분 = computed(() => {
 })
 const 휴게자동분 = computed(() => {
   const 체류 = 총체류분.value
-  if (체류 > 8 * 60) return 60
-  if (체류 > 4 * 60) return 30
+  if (체류 > 5 * 60) return 60
   return 0
 })
 const 휴게분 = computed(() => {
